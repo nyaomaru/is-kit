fd358e409a90ee8637ac5f39f6f0a3146a54ebf9
nyaomaru <nyaonyao0725@gmail.com>
2026-01-10 16:49:25 +0100

    chore: fix eslint setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 import pluginJs from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default defineConfig([
   pluginJs.configs.recommended,
@@ -8,7 +10,40 @@ export default defineConfig([
     rules: {
       'no-unused-vars': 'warn',
       'no-undef': 'warn',
-      complexity: ['error', { max: 7 }],
+      complexity: ['warn', { max: 7 }],
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-unsafe-function-type': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+  },
+  {
+    files: ['tests/**/*.ts', 'tests-d/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    files: ['tests-d/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 ]);
